e26b9fa2073702544a1dbfdf9b35e6fc692fa533
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2024-01-24 12:05:11 +0530

    Models Add

---
+++ backend/model/Hotel.js
@@ -0,0 +1,68 @@
+import mongoose from "mongoose";
+
+
+
+
+const hotelSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+
+    type: {
+        type: String,
+        required: true,
+    },
+
+    city: {
+        type: String,
+        required: true,
+    },
+
+    address: {
+        type: String,
+        required: true,
+    },
+
+    distances: {
+        type: String,
+        required: true,
+    },
+
+    photos: {
+        type: [String],
+        required: true,
+    },
+
+    desc: {
+        type: String,
+        required: true,
+    },
+
+    rating: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 5,
+    },
+
+    rooms: {
+        type: [String],
+        required: true,
+    },
+
+    cheapestRoom: {
+        type: Number,
+        required: true,
+    },
+
+    featured: {
+        type: Boolean,
+        required: true,
+        default: false,
+    }
+
+});
+
+
+export default mongoose.model("Hotel", hotelSchema);
