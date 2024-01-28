1969c5bce4a6aa20d8a4e9bede2b49904f0ad718
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2024-01-28 09:14:25 +0530

    Error MidlleWare Add

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,41 @@
 import express from "express";
-import dotenv from 'dotenv';
+import makeDataBaseConnection from "./database/data.js";
+import authRouter from "./routes/auth.js";
+import userRouter from "./routes/users.js";
+import roomRouter from "./routes/rooms.js";
+import hotelRouter from "./routes/hotels.js";
 
 
 
 
-dotenv.config();
+
 
 
 
 const app = express();
 const PORT = 5000;
 
+
+app.use(express.json());
+
+app.use((err, req, res, next) => {
+
+});
+
+
+
+app.use('/api/auth', authRouter);
+app.use('/api/hotel', hotelRouter);
+app.use('/api/user', userRouter);
+app.use('/api/room', roomRouter);
+
+
+
+
 app.listen(PORT, () => {
     console.log('Backend is Connected...!');
     console.log(`APP IS RUNNINNG ON PORT ${PORT}`);
+    makeDataBaseConnection();
 });
+
 
