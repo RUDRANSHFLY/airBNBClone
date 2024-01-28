7bb8b46ac06e01228e0842b2f5a3f7f816faec4c
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2024-01-28 08:54:22 +0530

    Hotel Contoller Attach

--- backend/routes/hotels.js
+++ backend/routes/hotels.js
@@ -1,10 +1,34 @@
 import expess from 'express';
+import {
+    createHotel,
+    deleteHotel,
+    getAllHotel,
+    getHotel,
+    updateHotel
+} from '../controller/hotel.js';
 
 const hotelRouter = expess.Router();
+/**
+ * ! CREATE
+ */
+hotelRouter.post("/", createHotel);
+/**
+ * ! UPDATE
+ */
+hotelRouter.put("/:id", updateHotel);
+/**
+ * ! DELETE
+ */
+hotelRouter.delete("/:id", deleteHotel);
+/**
+ * ! GET
+ */
+hotelRouter.get("/:id", getHotel);
+/**
+ * ! GET ALL 
+ */
+hotelRouter.get('/', getAllHotel);
 
 
-hotelRouter.get('/', (req, res) => {
-    res.send("This is hotel Endpoint..!");
-});
 
 export default hotelRouter;
