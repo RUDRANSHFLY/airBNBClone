adcd4a0da64f5829ddb45351bf4bb572550a101b
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2024-03-07 11:26:51 +0530

    Auth Controller Add

--- backend/controller/auth.js
+++ backend/controller/auth.js
@@ -1,6 +1,13 @@
 import User from "../model/User.js";
-import { generateEncryptedPassword } from "../utility/bcrypt.js";
+import { createError } from "../utility/Error.js";
+import { checkPassword, generateEncryptedPassword } from "../utility/bcrypt.js";
+import jwt from 'jsonwebtoken';
 
+
+
+/**
+ * * CREATE user Controller
+ */
 export const register = async (req, res, next) => {
     try {
         const newUser = new User({
@@ -10,9 +17,58 @@ export const register = async (req, res, next) => {
         });
 
         await newUser.save();
-
         res.status(201).json("USER has been Created......!");
+    } catch (error) {
+        next(error);
+    }
+}
 
+/**
+ * * LOG-IN user Controller
+ */
+
+export const login = async (req, res, next) => {
+    try {
+        const user = await User.findOne({ userName: req.body.userName });
+
+
+        if (!user) {
+            return next(createError(404, "User not found"))
+        }
+
+        const checkPasswordValidation = await checkPassword(req.body.password, user.password);
+
+
+
+        if (!checkPasswordValidation) {
+            return next(createError(
+                400,
+                "Wrong password or username......!"
+            ))
+        }
+
+
+
+        const { password, isAdmin, ...otherDetails } = user._doc;
+
+
+
+        const token = jwt.sign(
+            {
+                id: user._id,
+                isAdmin: user.isAdmin,
+            },
+            process.env.JWT
+        )
+
+
+        res.cookie(
+            "access_token",
+            token,
+            {
+                httpOnly: true,
+            }
+        ).status(200).json({ ...otherDetails });
 
     } catch (error) {
         next(error);
