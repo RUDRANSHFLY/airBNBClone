2606999ab43cd392fa2b4c22013139a80f5928d2
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2024-03-08 15:57:07 +0530

    feat:
     CRUD endpoints for creating, updating, deleting, rooms These endpoints allow for managing rooms in the system.

--- backend/routes/rooms.js
+++ backend/routes/rooms.js
@@ -1,11 +1,34 @@
 import express from 'express';
+import { createRoom, deleteRoom, getAllRooms, getRoom, updateRoom } from '../controller/room.js';
+import { verfiyAdmin } from '../utility/verifyToken.js';
+
+
+
 
 
 const roomRouter = express.Router();
 
 
-roomRouter.get('/', (req, res) => {
-    res.send("This is room Endpoint..!");
-})
+/**
+ * ! CREATE
+ */
+roomRouter.post('/:hotelId', verfiyAdmin, createRoom);
+/**
+ * ! UPDATE
+ */
+roomRouter.put("/:id", verfiyAdmin, updateRoom);
+/**
+ * ! DELETE
+ */
+roomRouter.delete("/:id/:hotelId", verfiyAdmin, deleteRoom);
+/**
+ * ! GET
+ */
+roomRouter.get('/:id', getRoom);
+/**
+ * ! GET - ALL
+ */
+roomRouter.get("/", getAllRooms);
+
 
 export default roomRouter;
