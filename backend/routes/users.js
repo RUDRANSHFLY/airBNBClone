f8ee42a3977a2d1d8e8f2f631d4adb07495030b6
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2024-03-08 15:57:17 +0530

    feat: Add user and authorization endpoints
    - Added routes for user authentication and authorization
    - Implemented token verification for authentication
    - Added middleware functions for verifying user and admin roles
    - Updated routes to require authentication and authorization for actions

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -6,9 +6,32 @@ import {
     getUser,
     updateUser
 } from '../controller/user.js';
+import { verfiyAdmin, verfiyUser, verifyToken } from '../utility/verifyToken.js';
+
+
+
+
 
 
 const userRouter = express.Router();
+/**
+ * ! VERIFY USER AUTHENTICATION
+ */
+userRouter.get("/checkAuthentication", verifyToken, (req, res, next) => {
+    res.send("Hello User u are Authenticated");
+});
+/**
+ * ! VERIFY USER 
+ */
+userRouter.get("/checkUserAuthentication/:id", verfiyUser, (req, res, next) => {
+    res.send("Hello User u are Logged In and have ACCESS to delete u r Account");
+})
+/**
+ * ! VERIFY IS_ADMIN
+ */
+userRouter.get("/checkisAdmin/:id", verfiyAdmin, (req, res, next) => {
+    res.send("Hello Admin u are logged in and have aceess to delete u r account and all acounts also");
+});
 /**
  * ! CREATE
  */
@@ -16,18 +39,18 @@ userRouter.post("/", createUser);
 /**
  * ! UPDATE
  */
-userRouter.put("/:id", updateUser);
+userRouter.put("/:id", verfiyUser, updateUser);
 /**
  * ! DELETE
  */
-userRouter.delete("/:id", deleteUser);
+userRouter.delete("/:id", verfiyUser, deleteUser);
 /**
  * ! GET
  */
-userRouter.get("/:id", getUser);
+userRouter.get("/:id", verfiyUser, getUser);
 /**
  * ! GET ALL 
  */
-userRouter.get('/', getAllUser);
+userRouter.get('/', verfiyAdmin, getAllUser);
 
 export default userRouter;
