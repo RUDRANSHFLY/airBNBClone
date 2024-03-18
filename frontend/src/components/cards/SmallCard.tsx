3896ee8e106edefb199a59ca17d7a3e11b0fd4a9
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2024-03-18 10:36:11 +0530

    feat: Add SmallCard component
    This commit adds the SmallCard component to the frontend/src/components/cards directory. The component allows users to view small cards with images, location, and distance information. The component also includes logic to display different images based on the location prop.

---
+++ frontend/src/components/cards/SmallCard.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Image from "next/image";
+import { manchesterImg, birkenheadImg } from "../../../utility/links";
+
+interface Props {
+  img: string;
+  location: string;
+  distance: string;
+}
+
+const SmallCard = ({ img, location, distance }: Props) => {
+  if (location === "Manchester") {
+    img = manchesterImg;
+  }
+
+  if (location === "Birkenhead") {
+    img = birkenheadImg;
+  }
+
+  return (
+    <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transistion transform duration-200 ease-out">
+      <div className="relative w-16 h-16">
+        <Image
+          src={img}
+          alt="exploreTypesPics"
+          className="rounded-lg"
+          layout="fill"
+        />
+      </div>
+      <div>
+        <h2>{location}</h2>
+        <h3>{distance}</h3>
+      </div>
+    </div>
+  );
+};
+
+export default SmallCard;
