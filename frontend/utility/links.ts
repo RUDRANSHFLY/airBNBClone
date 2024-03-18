dfc096d35a6887baf84f4a83fb75fba8d6869388
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2024-03-18 10:38:17 +0530

    feat: Add utility links for Airbnb website
    This links for image, and images for Manchester and Bir at end of file.

---
+++ frontend/utility/links.ts
@@ -0,0 +1,9 @@
+export const airBnbLogo : string ="https://links.papareact.com/qd3"
+
+export const airBnbIcon : string ="https://static-00.iconduck.com/assets.00/airbnb-icon-1902x2048-1ncv37ql.png";
+
+export const bannerImg : string = "https://links.papareact.com/0fm";
+
+export const manchesterImg : string = "https://upload.wikimedia.org/wikipedia/commons/6/6e/Bridgewater_Canal%2C_Castlefield_Basin_%28geograph_6966336%29.jpg";
+
+export const birkenheadImg : string = "https://media.istockphoto.com/id/627455126/photo/birkenhead-park-old-boat-house.jpg?s=612x612&w=0&k=20&c=Wift59FMp9Dpxs1Asd4EDwK4aw7kS1NXR_s3Wx0nGeI="
