691614df5b8d4934964d32285f576ddbc7662001
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2024-03-19 10:28:46 +0530

    feat: largeCardImg to links.ts

--- frontend/utility/links.ts
+++ frontend/utility/links.ts
@@ -7,3 +7,5 @@ export const bannerImg : string = "https://links.papareact.com/0fm";
 export const manchesterImg : string = "https://upload.wikimedia.org/wikipedia/commons/6/6e/Bridgewater_Canal%2C_Castlefield_Basin_%28geograph_6966336%29.jpg";
 
 export const birkenheadImg : string = "https://media.istockphoto.com/id/627455126/photo/birkenhead-park-old-boat-house.jpg?s=612x612&w=0&k=20&c=Wift59FMp9Dpxs1Asd4EDwK4aw7kS1NXR_s3Wx0nGeI="
+
+export const largeCardImg : string = "https://links.papareact.com/4cj";
